acc096fedf38d738812ed83ce3035d02a49607ec
Chris Paul <chrispaul3625@gmail.com>
2016-01-13 11:08:16 -0700

    Created milestone content and persona.php content

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>My personal web page.</title>
+
+	</head>
+	<header>
+		<nav>
+			<h1>Purpose, audience, goal documentation</h1>
+
+			<h2>Purpose</h2>
+			<p>The purpose of my webpage is to provide me with a professional online presence that will open doors to things like employment. </p>
+
+			<h3>Audience</h3>
+			<p>The people using my site would be potiental employers, other coders, and possibly recruiters. Clients/customers looking for contract work may also be viewing my website. </p>
+
+			<h4>Goal</h4>
+			<p>The goal of my personal web page is to help me gain full time employment with potiental employers and also help me gain exposure into the coding world for possible freelance work and networking.</p>
+
+			<a href="persona.php"> Persona Documentation</a>
+		</nav>
+	</header>
+
+
+	</body>
---
+++ public_html/documentation/persona.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Persona for my personal web page</title>
+	</head>
+	<body>
+		<header>
+			<h1>Persona Documentation</h1>
+
+			<h2>Name </h2>
+			<p>Jake Freemont</p>
+
+			<h3>Demographic Information</h3>
+
+			<p>Jake Freemont is a developer with a local start up company in Albuquere and is incharge of hiring at his company. He is 32 years old and is also a cofounder of the new start up. Jake is a laid back guy who enjoys the idea of working hard and playing hard. Jake has been a developer for 7 years and has been with the company since inception 3 years ago.</p>
+		</header>
+	</body>
+</html>
