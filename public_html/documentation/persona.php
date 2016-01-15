1bec501d48cd9b533bdfe8e9ceddb6b5d869ca0e
Chris Paul <chrispaul3625@gmail.com>
2016-01-15 09:11:07 -0700

    Created useset.php

--- public_html/documentation/persona.php
+++ public_html/documentation/persona.php
@@ -13,7 +13,9 @@
 
 			<h3>Demographic Information</h3>
 
-			<p>Jake Freemont is a developer with a local start up company in Albuquere and is incharge of hiring at his company. He is 32 years old and is also a cofounder of the new start up. Jake is a laid back guy who enjoys the idea of working hard and playing hard. Jake has been a developer for 7 years and has been with the company since inception 3 years ago.</p>
+			<p>Jake Freemont is a developer with a local start up company in Albuquere and is incharge of hiring at his company. He is 32 years old and is also a cofounder of the new start up. Jake is a laid back guy who enjoys the idea of working hard and playing hard. Jake has been a developer for 7 years and has been with the company since inception 3 years ago. Jake would visit my site to gain insight into what sort of experiences I have creating websites, learn more about me and also
+			to refrence experience listed on a resume with the experience I have listed on my personal website. Jake would be hoping to better set up an interview with me after exploring my website and also be able to formulate
+			more detailed and specific questions for me during an interview.</p>
 
 			<a href="milestone-1.php"> Purpose, audience, goal documentation </a>
 
---
+++ public_html/documentation/usecase.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title> Use Case Documentation for my personal web page</title>
+	</head>
+	<body>
+		<header>
+			<h1>Milestone-1 Use Case Documentation </h1>
+			<p>  </p>
+			<a href="milestone-1.php"> Purpose, audience, goal documentation </a>
+
+		</header>
+	</body>
